9800f1581e52b4b4eb25eb05b081e0e404e93cf8
Rafa Bello <rafabp986@outlook.com>
2025-10-16 12:50:42 +0200

    create factories

---
+++ cocktails_api/database/factories/CocktailFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Cocktail>
+ */
+class CocktailFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->unique()->word(),
+            'description' => fake()->sentence(10),
+            'type' => fake()->randomElement(['alcoholic', 'non-alcoholic']),
+        ];
+    }
+}
