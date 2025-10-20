e3b56ac870f402a450014effc07e0c3882eff3c3
Rafa Bello <rafabp986@outlook.com>
2025-10-20 11:04:10 +0200

    routes of controllers

--- cocktails_api/routes/api.php
+++ cocktails_api/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CocktailController;
+use App\Http\Controllers\Api\IngredientController;
 
 // públicas
 Route::post('/register', [AuthController::class, 'register']);
@@ -12,4 +14,10 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->group(function () {
     Route::get('/users/{id}', [AuthController::class, 'profile']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+// CRUD Cocktails
+Route::apiResource('cocktails', CocktailController::class);
+
+// CRUD Ingredients
+Route::apiResource('ingredients', IngredientController::class);
 });
