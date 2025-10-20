a4f0945d784b1776a70f4cf8722c54c8f5378f77
Rafa Bello <rafabp986@outlook.com>
2025-10-20 10:21:06 +0200

    create ingredient and cocktail controller, are empty

--- cocktails_api/app/Providers/AuthServiceProvider.php
+++ cocktails_api/app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -16,5 +17,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->registerPolicies();
+
+        //Passport::ignoreMigrations();
     }
 }
--- cocktails_api/routes/api.php
+++ cocktails_api/routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+// públicas
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// protegidas (requieren token)
+Route::middleware('auth:api')->group(function () {
+    Route::get('/users/{id}', [AuthController::class, 'profile']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
